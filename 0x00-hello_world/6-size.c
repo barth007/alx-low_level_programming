539b405e737656cfb0e3873efd1c71d000eb213f
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2022-10-17 20:42:21 -0700

    made correction to the 6-size.c script as well as the respective executable files

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ int main(void)
 	float float_type;
 
 	printf("Size of a char: %zu byte(s)\n", sizeof(char_type));
-	printf("Size of a int: %zu byte(s)\n", sizeof(int_type));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int_type));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long_int_type));
 	printf("Size of a long long int: %zu byte(s)\n",
 	sizeof(long_long_int_type));
