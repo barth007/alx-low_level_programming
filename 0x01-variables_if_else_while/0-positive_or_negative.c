444af63a242b87d97f45a91f0825b2c3a2ae6923
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2022-10-19 10:03:00 -0700

    applied betty rules

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,9 +18,9 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/*checking the value*/
-	if(n<0)
+	if (n < 0)
 		printf("%d is negative\n", n);
-	else if(n>0)
+	else if (n > 0)
 		printf("%d is positive\n", n);
 	else
 		printf("%d is zero\n", n);
