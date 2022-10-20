db43c83ac2f4175cecf4af216a7f652027ad7c03
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2022-10-20 06:25:06 -0700

    added the 2-print_alphabet.c, 2-print_alphabet script and executable file respectively

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/* more header come in here*/
+
+
+/**
+ * main - Entry  point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
