ecc3e5dd3dbce101689334b828282747a6986a9c
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2022-10-20 07:43:34 -0700

    added 5-print_numbers.c script and 5-print_numbers executable file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/*more headers here*/
+
+/**
+ * main - Entry - point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	/*variable initializition*/
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
