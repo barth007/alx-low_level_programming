2e69fcb97df4acfd8d8b214d3d67569c5a1911ba
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2022-10-27 09:53:25 -0700

    added task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+/**
+ * char *_strncpy - function copies a string
+ * @dest: copy to
+ * @src: copied from
+ * @n: number of bytes from src
+ * Return: string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+		if (src[i] == '\0')
+			i = n;
+	}
+	return (dest);
+}
