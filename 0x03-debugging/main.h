781c3dbe6b153eeecb8bda2431bc31b7ab418151
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2022-10-30 11:50:43 -0700

    done with first task

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+	void positive_or_negative(int);
+	int largest_number(int, int, int);
+	int convert_day(int, int);
+	void print_remaining_days(int, int, int);
+#endif
