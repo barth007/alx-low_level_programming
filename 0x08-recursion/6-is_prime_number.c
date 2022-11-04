9715c8ae07ced589d596bc64244e66282abf63a2
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2022-11-03 22:32:17 -0700

    added task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,20 +1,30 @@
+/**
+ * find_prime - checks if a number is prime
+ * @num: the number to be checked
+ * @i: checks if i is a multiple of num
+ * Return: 1 if prime otherwise 0
+ */
 int find_prime(int num, int i)
 {
-	if ((num % i == 0) && (num != i))
+	if (num % i == 0)
 		return (0);
-	if (num == i)
+	if (i < num / 2)
 	{
-		return (1);
+		i++;
+		return (find_prime(num, i));
 	}
-	return (find_prime(num, i + 1));
-
+	return (1);
 }
-
+/**
+ * is_prime_number - function calls the find_prime()
+ * @n: number to be checked
+ * Return: output of find_prime
+ */
 int is_prime_number(int n)
 {
 	int i;
-	
-	i = 1;
+
+	i = 2;
 	if (n < 0 || n == 1)
 		return (0);
 	return (find_prime(n, i));
