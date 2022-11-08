9233eaf2f5236c55ddb40c8802773c634c34d14a
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2022-11-08 13:11:38 -0800

    checked task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	else if (size > 0)
 	{
-		arr = malloc(size  * sizeof(c));
+		arr = malloc(size  * sizeof(char));
 		for (i = 0; i < size; i++)
 		{
 			arr[i] = c;
