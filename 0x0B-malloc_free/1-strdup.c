d0d0daae5566ff68c37a88cde376604fa2ab0a51
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-01-10 12:10:11 -0800

    3rd updated the 1-strdup.c file

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -18,7 +18,7 @@ char *_strdup(char *str)
 	len = 0;
 	for (i = 0; str[i] != '\0'; i++)
 		len++;
-	cpy = malloc(len * sizeof(char));
+	cpy = malloc(len * sizeof(char) + 1);
 	if (cpy)
 	{
 		for (j = 0; j <= len; j++)
