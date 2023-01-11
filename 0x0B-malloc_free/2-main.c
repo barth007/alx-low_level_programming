2888adcf3a42b717bcd66732e697b0e863b8dd26
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-01-11 03:25:20 -0800

    added the 2-str_concat.c file

--- 0x0B-malloc_free/2-main.c
+++ 0x0B-malloc_free/2-main.c
@@ -6,7 +6,7 @@ int main (void)
 {
 	char *s;
 
-	s = str_concat("Bettty ", "Holberton");
+	s = str_concat("Betty", "Holberton");
 	if (s == NULL)
 	{
 		printf("failed\n");
--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -43,6 +43,10 @@ char *str_concat(char *s1, char *s2)
 
 	if ((s1 == NULL) & (s2 == NULL))
 		return (NULL);
+	if ((s1 != NULL) & (s2 == NULL))
+		return (s1);
+	if ((s2 != NULL) & (s1 == NULL))
+		return (s2);
 	len1 = 0;
 	for (i = 0; s1[i] != '\0'; i++)
 		len1++;
