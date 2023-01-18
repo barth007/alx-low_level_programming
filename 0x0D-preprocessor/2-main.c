d740aaa6d4f36708ee80e1191d6a8954003f4a5d
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-01-17 20:48:09 -0800

    added file 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - entry point print the name of file compile with
+ * Return: Always return (0)
+**/
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
