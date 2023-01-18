872bf76710ec449b48a908698d8561cc943a8e8d
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-01-17 19:28:04 -0800

    corrected task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,4 +1,6 @@
 #include "dog.h"
+#include <math.h>
+#include <string.h>
 /**
  * new_dog - a function that creates a new dog.
  * @name: parameter to the function
@@ -10,11 +12,26 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *info;
 
+	if (name == NULL || isnan(age) || owner == NULL)
+		return (NULL);
 	info = malloc(sizeof(dog_t));
 	if (!info)
 		return (NULL);
-	info->name = name;
+
+	info->name = malloc(sizeof(strlen(name) + 1));
+	if (info->name == NULL)
+	{
+		return (NULL);
+		free(info);
+	}
+	strcpy(info->name, name);
 	info->age = age;
-	info->owner = owner;
+	info->owner = malloc(sizeof(strlen(owner) + 1));
+	if (info->owner == NULL)
+	{
+		return (NULL);
+		free(info);
+	}
+	strcpy(info->owner, owner);
 	return (info);
 }
