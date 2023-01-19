b156a9f61422afb449dd6f0d17730bd85699f147
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-01-19 15:37:17 -0800

    added the task 3 and correction of task 2

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,17 +11,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list valist;
 	unsigned int i;
 
-	if (separator == NULL || n == 0)
-		exit(EXIT_FAILURE);
 	va_start(valist, n);
 	for (i = 0; i < n; i++)
 	{
-		if (i == n - 1)
-		{
-			printf("%d\n", va_arg(valist, int));
-			break;
-		}
-		printf("%d%s ", va_arg(valist, int), separator);
+		printf("%d", va_arg(valist, int));
+		if (separator && i < n - 1)
+			printf("%s ", separator);
 	}
+	printf("\n");
 	va_end(valist);
 }
---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - function that prints strings, followed by a new line.
+ * @separator: string parameter
+ * @n: number of argument passed
+ * @...: variable argument
+**/
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int i;
+	char *str;
+
+	va_start(valist, n);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(valist, char*);
+		if (str)
+			printf("%s", str);
+		else
+			printf("(nil)");
+		if (separator && i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+}
