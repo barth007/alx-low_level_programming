1a8f7134aff67ad2f343980b6b4207245d77f376
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-01-19 15:52:02 -0800

    corrected task 2

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,9 +14,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(valist, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(valist, int));
-		if (separator && i < n - 1)
-			printf("%s ", separator);
+	if (!separator)
+		exit(EXIT_FAILURE);
+	printf("%d", va_arg(valist, int));
+	if (i < n - 1)
+		printf("%s ", separator);
 	}
 	printf("\n");
 	va_end(valist);
