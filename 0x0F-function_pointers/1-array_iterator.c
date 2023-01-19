619818b43cbc4030e7aa7f2ed6891331bcc9532e
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-01-19 02:17:16 -0800

    corrected task 2

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -5,11 +5,14 @@
  * @array: an array of integers
  * @size: size of array
  * @action: function pointer that takes an int datatype as arg.
+ * exit: return failure if function fail
 **/
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
+	if (size <= 0)
+		exit(EXIT_FAILURE);
 	for (i = 0; i < size; i++)
 		action(array[i]);
 }
