0f4c64ef2876f9d453c33f0a494b41283708db2a
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-01-20 17:07:56 -0800

    corrected task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,25 +2,30 @@
 /**
  * int_index - function takes argument as an array, size of the array
  * and function pointer
+ *
  * @array: argument of an array
  * @size: size of an array
- * @comp: function pointer taking an integer argument
+ * @cmp: function pointer taking an integer argument
+ * Return: return an integer
 **/
 int int_index(int *array, int size, int (*cmp)(int))
 {
-        int i, index;
+	int i, index;
 
 	index = -1;
 	if (size <= 0)
 		return (-1);
-        for (i = 0; i < size; i++)
-        {
-                if (cmp(array[i]) != 0)
-                {
-			index = i;
-			break;
+	for (i = 0; i < size; i++)
+	{
+		if (cmp && array)
+		{
+			if (cmp(array[i]) != 0)
+			{
+				index = i;
+				break;
+			}
 		}
-        }
+	}
 	if (index < 0)
 		return (-1);
 	return (index);
