80d5bf66d4848944068c20a4120a3b286847b8aa
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-01-22 08:41:54 -0800

    done with task 3

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,15 +11,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list valist;
 	unsigned int i, _num;
 
-	if (separator == NULL)
-		return;
 	va_start(valist, n);
 	for (i = 0; i < n; i++)
 	{
 		_num = va_arg(valist, const unsigned int);
 		printf("%d", _num);
+		if (separator == NULL)
+			return;
 		if (i != n - 1)
-			printf("%s ", separator);
+			printf("%s", separator);
 	}
 	printf("\n");
 	va_end(valist);
