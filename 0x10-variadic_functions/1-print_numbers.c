5258930b8497a46dab63a70b561257e8157a73c3
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-01-21 21:54:41 -0800

    corrected the task 2

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,8 +11,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list valist;
 	unsigned int i, _num;
 
-	if (n <= 0)
-		return;
 	va_start(valist, n);
 	for (i = 0; i < n; i++)
 	{
