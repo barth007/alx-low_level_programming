d38a6a1e292fd4540b7c7d5bf47a4b65802c6bc1
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-01-31 12:44:34 +0000

    added task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ *free_listint2 - frees up the list
+ *@head: incoming node
+**/
+void free_listint2(listint_t **head)
+{
+	listint_t *free_node;
+	listint_t *last_node;
+
+	free_node = *head;
+	while (free_node != NULL)
+	{
+		last_node = free_node;
+		free_node = free_node->next;
+		free(last_node);
+	}
+}
