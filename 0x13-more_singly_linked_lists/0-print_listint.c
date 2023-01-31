b8f73a8f9ce8bf6a63e56279121f22c1540cf2da
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-01-31 00:19:55 +0000

    added the README.md file and task0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ *print_listint - prints all elements of a listint_t list.
+ *@h: a string pointer to listint_t
+ *Return: length of node
+**/
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *current;
+	size_t counter;
+
+	current = h;
+	counter = 0;
+	while (current)
+	{
+		printf("%ld\n", current->n);
+		current = current->next;
+		counter++;
+	}
+	return (counter);
+
+}
---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,19 @@
+#ifndef HEADER_H
+#define HEADER_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+/**
+ *struct node - a user defined data containing a data and next node
+ *@data: this is a string pointing to a char
+ *@next: this is the next node to point to
+**/
+typedef struct node
+{
+	int data;
+	size_t n;
+	struct node *next;
+} listint_t;
+size_t print_listint(const listint_t *h);
+#endif
