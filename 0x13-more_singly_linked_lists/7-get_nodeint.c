f22181fd772d0fb49a2025ba7e6d2f8452bc17ac
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-02-01 02:01:43 +0000

    added the task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,7 +14,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		return (NULL);
 	temp = head;
 	counter = 0;
-	while (temp->next != NULL)
+	while (temp != NULL)
 	{
 		if (counter == index)
 		{
