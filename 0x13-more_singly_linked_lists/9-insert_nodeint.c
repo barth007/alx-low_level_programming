e1341b32c4b1a55b61c24589610bc93a7a9c8dc4
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-02-01 04:45:16 +0000

    added task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,14 +20,18 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	ptr2->next = NULL;
 	ptr = *head;
 	tracker = 0;
-	idx--;
-	while (idx != 1)
+	while (idx != 1 && ptr->next != NULL)
 	{
 		ptr = ptr->next;
 		idx--;
 		tracker++;
 	}
-	ptr2->next = ptr->next;
-	ptr->next = ptr2;
-	return (ptr);
+	if (tracker <= idx - 2)
+	{
+		ptr2->next = ptr->next;
+		ptr->next = ptr2;
+		return (ptr2);
+	}
+	else
+		return (NULL);
 }
