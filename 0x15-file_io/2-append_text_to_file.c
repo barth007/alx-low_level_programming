5f40ff535b98fcad955c0acaaab679bdae0e0f9a
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-02-10 00:37:21 +0000

    added task 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *append_text_to_file - function that appends to a file
+ *@filename: the file to append to
+ *@text_content: the content to append to the file
+ *Return: 1
+**/
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file_descriptor, i;
+
+	if (filename == NULL)
+		return (-1);
+	file_descriptor = open(filename, O_WRONLY | O_APPEND);
+	if (file_descriptor == -1)
+		return (-1);
+	i = 0;
+	while (text_content[i])
+		i++;
+	if (text_content == NULL)
+	{
+		close(file_descriptor);
+		return (1);
+	}
+	write(file_descriptor, text_content, i);
+	close(file_descriptor);
+	return (1);
+}
