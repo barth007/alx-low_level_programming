ebdcdf8c8596da6cc0c02b1e9aac2d5f819da377
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-02-11 00:05:48 +0000

    corrected task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,40 +1,62 @@
 #include "main.h"
 /**
- *main - copies one file to another file
+ *cp_file - copy from one file to another
+ *@file_from: file to be copied
+ *@file_to: destination file to copy to
+ *Return: return copied file
+**/
+size_t cp_file(char *file_from, char *file_to)
+{
+	int fd_read, fd_write;
+	void *buffer[BUFSIZE];
+	size_t byte_read, write_byte;
+
+	fd_read = open(file_from, O_RDONLY);
+	if (fd_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read to %s\n", file_from);
+		exit(98);
+	}
+	fd_write = open(file_to, O_CREAT | O_TRUNC | O_WRONLY | O_RDONLY, 0664);
+	if (fd_write == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		exit(99);
+	}
+	while ((byte_read = read(fd_read, buffer, BUFSIZE)) > 0)
+	{
+		write_byte = write(fd_write, buffer, byte_read);
+		if (write_byte == (size_t)-1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+			exit(99);
+		}
+	}
+	if (close(fd_write) == -1)
+	{
+		dprintf(STDERR_FILENO, "Can't close fd %ld\n", write_byte);
+		exit(100);
+	}
+	if (close(fd_read) == -1)
+	{
+		dprintf(STDERR_FILENO, "Can't close fd %ld\n", byte_read);
+		exit(100);
+	}
+	return (fd_write);
+}
+/**
+ *main - Entry point
  *@argc: argument count
  *@argv: an array
  *Return: 0
 **/
 int main(int argc, char *argv[])
 {
-	int file_from, file_to;
-	void *buffer[BUFSIZE];
-	size_t  byte_read, write_byte;
-
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
 		exit(97);
 	}
-	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY | O_RDONLY, 0664);
-	while ((byte_read = read(file_from, buffer, BUFSIZE)) > 0)
-	{
-		write_byte = write(file_to, buffer, byte_read);
-		if (write_byte == (size_t)-1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
-	}
-	if (close(file_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Can't close fd %ld\n", write_byte);
-		exit(100);
-	}
-	if (close(file_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Can't close fd %ld\n", byte_read);
-		exit(100);
-	}
-	return (0); }
+	cp_file(argv[1], argv[2]);
+	return (0);
+}
