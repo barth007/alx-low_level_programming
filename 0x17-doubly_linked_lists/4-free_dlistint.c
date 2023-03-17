af86df964ed35cc8f95bbf9cd0fb1e594fdb9fe2
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-03-17 08:15:05 +0000

    added task 4 file

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ *free_dlistint - frees a dlistint_t list
+ *@head: the node to be freed
+**/
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *free_nodes;
+	dlistint_t *last_nodes;
+
+	if (head == NULL)
+		return;
+	free_nodes = head;
+	while (free_nodes !=NULL)
+	{
+		last_nodes = free_nodes;
+		free_nodes = free_nodes->next;
+		free(last_nodes);
+	}
+	head = NULL;
+}
