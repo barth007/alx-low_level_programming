d30f466b2a0bc1995344a8774aa83965691288eb
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-03-18 01:23:25 +0000

    added task 7 file

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,33 +11,32 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *temp, *new_node;
 	unsigned int tracker;
 
+	if (*h == NULL)
+		return (NULL);
 	tracker = 0;
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
 		return (NULL);
 	new_node->n = n;
 	new_node->prev = NULL;
-	if (*h)
+	temp = *h;
+	if (idx == 0)
 	{
-		temp = *h;
-		if (idx == 0)
-		{
-			new_node->next = *h;
-			(*h)->prev = new_node;
-			(*h) = new_node;
-			return (*h);
-		}
-		while (tracker != idx - 1 && temp->next != NULL)
-		{
-			temp = temp->next;
-			tracker++;
-		}
-		if (tracker == idx - 1)
-		{
-			new_node->next = temp->next;
-			temp->next = new_node;
-			return (new_node);
-		}
+		new_node->next = *h;
+		(*h)->prev = new_node;
+		(*h) = new_node;
+		return (*h);
+	}
+	while (tracker != idx - 1 && temp->next != NULL)
+	{
+		temp = temp->next;
+		tracker++;
+	}
+	if (tracker == idx - 1)
+	{
+		new_node->next = temp->next;
+		temp->next = new_node;
+		return (new_node);
 	}
 	return (NULL);
 }
