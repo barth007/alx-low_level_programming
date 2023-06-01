f3322828329ce5dbd3bb3932fbe62c6388e60832
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-06-01 13:48:18 +0000

    updated the documentation of  the function

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,6 @@
 #include "function_pointers.h"
 /**
- * print_name -function takes two argument string pointer and function pointer
+ * print_name -function prints a name.
  * @name: argument pointer to the string
  * @f: function pointer that takes a string pointer to char data type
  * exit: exit from the code if string argument is NULL
