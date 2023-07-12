f1007ede36cabcf525e278adfebdb8381533ca57
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-07-12 14:23:18 +0000

    added the 0 task

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# This script sends a request to that URL, and displays the size of the body of
+# the response
+curl -I "$1" | grep "content-length";
---
+++ 0x10-python-network_0/README.md
@@ -0,0 +1,1 @@
+This is ont 0x10. Python - Network #0
